66124facef268ff2cf111e49548d294f525bbdb6
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2025-09-05 22:20:53 +0200

    Better tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,28 +5,18 @@ using Aqua
 @testset "UnsetIndex.jl" begin
     map_isassigned(A::AbstractArray) = map(i -> isassigned(A, i), eachindex(A))
     @test string(unset) == "#undef"
-    @testset "T = $T" for T in (String, Any)
-        A = T["Hello", "new", "world!"]
-        @test map_isassigned(A) == [true, true, true]
-        @test (@inferred unsetindex!(A, 1)) === A
-        @test map_isassigned(A) == [false, true, true]
-        A[end] = unset
-        @test map_isassigned(A) == [false, true, false]
-        A[2] = unset
-        @test map_isassigned(A) == [false, false, false]
-        if isdefined(Base, :Memory) # Memory appears in Julia 1.11
-            B = Memory{T}(undef, 3)
-            B[1] = "This"
-            B[2] = "is"
-            B[3] = "wonderful!"
-            @test map_isassigned(B) == [true, true, true]
-            @test (@inferred unsetindex!(B, 1)) === B
-            @test map_isassigned(B) == [false, true, true]
-            B[end] = unset
-            @test map_isassigned(B) == [false, true, false]
-            B[2] = unset
-            @test map_isassigned(B) == [false, false, false]
-        end
+    words = split("This is a wonderful world indeed!")
+    arraytypes = (Array,)
+    if isdefined(Base, :Memory) # Memory appears in Julia 1.11
+        arraytypes = (arraytypes..., Memory)
+    end
+    @testset "$A{$T}" for A in arraytypes, T in (String, Any)
+        X = A{T}(undef, length(words))
+        copyto!(X, words)
+        @test (@inferred unsetindex!(X, 1)) === X
+        @test map_isassigned(X) == [false, true, true, true, true, true]
+        X[end] = unset
+        @test map_isassigned(X) == [false, true, true, true, true, false]
     end
 end
 
